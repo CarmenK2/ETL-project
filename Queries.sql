5e5e1283239285f3a0a49b6a4a96135814110397
l-Josh-Thomas-l <josh.thomas795@gmail.com>
2022-06-13 22:26:53 +0800

    made updates

--- Queries.sql
+++ Queries.sql
@@ -3,113 +3,112 @@
 
 
 CREATE TABLE "Winery_Location" (
-    "Winery" varchar   NOT NULL,
-    "Latitude" float   NOT NULL,
-    "Longitude" float   NOT NULL,
-    "Closest_weather_station" int   NOT NULL,
-    "Name_of_weather_station" varchar   NOT NULL,
+    "Winery" varchar    ,
+    "Latitude" float    ,
+    "Longitude" float    ,
+    "Closest_weather_station" int    ,
+    "Name_of_weather_station" varchar    ,
     CONSTRAINT "pk_Winery_Location" PRIMARY KEY (
         "Winery"
      )
 );
 
 CREATE TABLE "Weather_Average_Rainfall" (
-    "Closest_weather_station" int   NOT NULL,
-    "Average_yearly_rainfall" float   NOT NULL,
+    "Closest_weather_station" int    ,
+    "Average_yearly_rainfall" float    ,
     CONSTRAINT "pk_Weather_Average_Rainfall" PRIMARY KEY (
         "Closest_weather_station"
      )
 );
 
 CREATE TABLE "Weather_Average_Solar" (
-    "Closest_weather_station" int   NOT NULL,
-    "Average_yearly_solar_exposure" float   NOT NULL,
+    "Closest_weather_station" int    ,
+    "Average_yearly_solar_exposure" float    ,
     CONSTRAINT "pk_Weather_Average_Solar" PRIMARY KEY (
         "Closest_weather_station"
      )
 );
 
 CREATE TABLE "Soil_data" (
-    "Winery" varchar   NOT NULL,
-    "Latitude" float   NOT NULL,
-    "Longitude" float   NOT NULL,
-    "SubSoil_Condition" varchar   NOT NULL,
-    "Soil_Type" varchar   NOT NULL,
+    "Winery" varchar    ,
+    "Latitude" float    ,
+    "Longitude" float    ,
+    "SubSoil_Condition" varchar    ,
+    "Soil_Type" varchar    ,
     CONSTRAINT "pk_Soil_data" PRIMARY KEY (
         "Winery"
      )
 );
 
 CREATE TABLE "Winery_Export_Sales_by_Geolocation" (
-    "Region" varchar   NOT NULL,
-    "2021_Export_Sales" varchar   NOT NULL,
-    "2020_Export_Sales" varchar   NOT NULL,
-    "2019_Export_Sales" varchar   NOT NULL,
-    "2018_Export_Sales" varchar   NOT NULL,
-    "2017_Export_Sales" varchar   NOT NULL,
-    "2016_Export_Sales" varchar   NOT NULL,
-    "2015_Export_Sales" varchar   NOT NULL,
-    "2014_Export_Sales" varchar   NOT NULL,
-    "2013_Export_Sales" varchar   NOT NULL,
+    "Region" varchar    ,
+    "2021_Export_Sales" varchar    ,
+    "2020_Export_Sales" varchar    ,
+    "2019_Export_Sales" varchar    ,
+    "2018_Export_Sales" varchar    ,
+    "2017_Export_Sales" varchar    ,
+    "2016_Export_Sales" varchar    ,
+    "2015_Export_Sales" varchar    ,
+    "2014_Export_Sales" varchar    ,
+    "2013_Export_Sales" varchar    ,
     CONSTRAINT "pk_Winery_Export_Sales_by_Geolocation" PRIMARY KEY (
         "Region"
      )
 );
 
 CREATE TABLE "Wine_volume_by_region" (
-    "Region" varchar   NOT NULL,
-    "Year" int   NOT NULL,
-    "Volume_(L)" float   NOT NULL,
-    "Average_Sales_Value_per_Liter" float   NOT NULL,
+    "Region" varchar    ,
+    "Year" int    ,
+    "Volume_(L)" float    ,
+    "Average_Sales_Value_per_Liter" float    ,
     CONSTRAINT "pk_Wine_volume_by_region" PRIMARY KEY (
         "Region"
      )
 );
 
 CREATE TABLE "Wine_Grape_variety_Sales" (
-    "Grape_Variety" varchar   NOT NULL,
-    "2021_Export_Sales" varchar   NOT NULL,
-    "2020_Export_Sales" varchar   NOT NULL,
-    "2019_Export_Sales" varchar   NOT NULL,
-    "2018_Export_Sales" varchar   NOT NULL,
-    "2017_Export_Sales" varchar   NOT NULL,
-    "2016_Export_Sales" varchar   NOT NULL,
-    "2015_Export_Sales" varchar   NOT NULL,
-    "2014_Export_Sales" varchar   NOT NULL,
-    "2013_Export_Sales" varchar   NOT NULL,
-    "2012_Export_Sales" varchar   NOT NULL,
+    "Grape_Variety" varchar    ,
+    "2021_Export_Sales" varchar    ,
+    "2020_Export_Sales" varchar    ,
+    "2019_Export_Sales" varchar    ,
+    "2018_Export_Sales" varchar    ,
+    "2017_Export_Sales" varchar    ,
+    "2016_Export_Sales" varchar    ,
+    "2015_Export_Sales" varchar    ,
+    "2014_Export_Sales" varchar    ,
+    "2013_Export_Sales" varchar    ,
+    "2012_Export_Sales" varchar    ,
     CONSTRAINT "pk_Wine_Grape_variety_Sales" PRIMARY KEY (
         "Grape_Variety"
      )
 );
 
 CREATE TABLE "Wine_Grape_varierty_Production" (
-    "Region" varchar   NOT NULL,
-    "Year" int   NOT NULL,
-    "Wine_Colour" varchar   NOT NULL,
-    "Grape_Variety" varchar   NOT NULL,
-    "Tonnes_purchased" float   NOT NULL,
-    "Total_value_purchased" float   NOT NULL,
-    "Average_purchase_value_per_tonne" float   NOT NULL,
-    "Winery_grown_fruit_per_tonne" float   NOT NULL,
-    "Winery_grown_share_of_crush" float   NOT NULL,
-    "Total_crushed_(T)" float   NOT NULL,
-    "Total_value_all_grapes" float   NOT NULL,
+    "Region" varchar    ,
+    "Year" int    ,
+    "Wine_Colour" varchar    ,
+    "Grape_Variety" varchar    ,
+    "Tonnes_purchased" float    ,
+    "Total_value_purchased" float    ,
+    "Average_purchase_value_per_tonne" float    ,
+    "Winery_grown_fruit_per_tonne" float    ,
+    "Winery_grown_share_of_crush" float    ,
+    "Total_crushed_(T)" float    ,
+    "Total_value_all_grapes" float    ,
     CONSTRAINT "pk_Wine_Grape_varierty_Production" PRIMARY KEY (
         "Region"
      )
 );
 
 CREATE TABLE "Winery_Tasting_Price_data" (
-    "Winery" varchar   NOT NULL,
-    "Region" varchar   NOT NULL,
-    "Wine_Name" varchar   NOT NULL,
-    "Grape_Variety" varchar   NOT NULL,
-    "Tasting_Score" int   NOT NULL,
-    "Price" int   NOT NULL,
-    CONSTRAINT "pk_Winery_Tasting_Price_data" PRIMARY KEY (
-        "Winery"
-     )
+    "Winery" varchar    ,
+    "Region" varchar    ,
+    "Wine_Name" varchar    ,
+    "Grape_Variety" varchar    ,
+    "Tasting_Score" int    ,
+    "Price" int    ,
+    "ID" serial primary key
+     
 );
 
 ALTER TABLE "Winery_Location" ADD CONSTRAINT "fk_Winery_Location_Winery" FOREIGN KEY("Winery")
